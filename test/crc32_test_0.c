9902de91ae65875130a224e1120e278fd558dee9
omemisoglu-testsan <oguzhan@testsan.com.tr>
2020-06-18 16:25:54 +0300

    Test Argument Handling, Makefile

--- test/crc32_test_0.c
+++ test/crc32_test_0.c
@@ -15,11 +15,21 @@
 //
 #include "../crc32.h"
 
+// Relative path to README File
+//
+#define FILE_TEST_PATH   "../README.md"
+
 int main(int argc, char * argv[])
 {
     int i;
-
-    char * file_name_t = argv[1];
+    char * file_name_t;
+    if (argc > 1)
+        file_name_t = argv[1];
+    else
+    {
+        char file_test[] = FILE_TEST_PATH;
+        file_name_t = file_test;
+    }
 
     uint32_t crc_results[8];
     size_t crc_bufs[] = {2048, 1, 12, 55, 1023, 1024, 19, 0};
@@ -40,7 +50,7 @@ int main(int argc, char * argv[])
     }
 
     if (is_success)
-        printf("Success");
+        printf("Success: %08x", crc_results[0]);
     else
     {
         printf("Fail");
